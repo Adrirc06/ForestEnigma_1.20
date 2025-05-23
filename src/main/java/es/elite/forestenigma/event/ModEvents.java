0643fd58bd76fbdf432cba7b0b4f25d9978c9644
Adrirc06 <adrianrocu2006@gmail.com>
2025-05-23 11:02:43 +0200

    Revert "añadidos los archivos que tenia en la version 1.21 con arreglos"
    This reverts commit 22b84b430449d943c8f8a703d5e69a2fbe92df94.

--- src/main/java/es/elite/forestenigma/event/ModEvents.java
+++ src/main/java/es/elite/forestenigma/event/ModEvents.java
@@ -6,6 +6,7 @@ import es.elite.forestenigma.villages.ModVillagers;
 import net.minecraft.world.entity.npc.VillagerProfession;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.ItemCost;
 import net.minecraft.world.item.trading.MerchantOffer;
 import net.minecraftforge.event.village.VillagerTradesEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -18,45 +19,33 @@ public class ModEvents {
     public static void addCustomTrades(VillagerTradesEvent event) {
         if(event.getType() == ModVillagers.KAUPENGER.get()) {
             var trades = event.getTrades();
-            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.EMERALD, 5),
-                    ItemStack.EMPTY,
-                    new ItemStack(ModItems.SYLVANITE.get(), 4),
-                    6, 4, 0.05f));
 
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.DIAMOND, 3),
-                    ItemStack.EMPTY,
-                    new ItemStack(ModItems.SYLVANITE.get(), 10),
-                    6, 4, 0.05f));
+                    new ItemCost(Items.EMERALD, 5),
+                    new ItemStack(ModItems.SYLVANITE.get(), 4), 6, 4, 0.05f));
+
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemCost(Items.DIAMOND, 3),
+                    new ItemStack(ModItems.SYLVANITE.get(), 10), 6, 4, 0.05f));
 
             trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(ModItems.SYLVANITE.get(), 9),
-                    ItemStack.EMPTY,
-                    new ItemStack(Items.BELL, 1),
-                    1, 12, 0.05f));
-
+                    new ItemCost(ModItems.SYLVANITE.get(), 9),
+                    new ItemStack(Items.BELL, 1), 1, 12, 0.05f));
         }
         if(event.getType() == VillagerProfession.FARMER) {
             var trades = event.getTrades();
-            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.EMERALD, 5),
-                    ItemStack.EMPTY,
-                    new ItemStack(ModItems.SYLVANITE.get(), 4),
-                    6, 4, 0.05f));
 
             trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(Items.DIAMOND, 3),
-                    ItemStack.EMPTY,
-                    new ItemStack(ModItems.SYLVANITE.get(), 10),
-                    6, 4, 0.05f));
+                    new ItemCost(Items.EMERALD, 5),
+                    new ItemStack(ModItems.SYLVANITE.get(), 14), 6, 4, 0.05f));
+
+            trades.get(1).add((pTrader, pRandom) -> new MerchantOffer(
+                    new ItemCost(Items.DIAMOND, 3),
+                    new ItemStack(Items.ARROW, 32), 6, 4, 0.05f));
 
             trades.get(2).add((pTrader, pRandom) -> new MerchantOffer(
-                    new ItemStack(ModItems.SYLVANITE.get(), 9),
-                    ItemStack.EMPTY,
-                    new ItemStack(Items.BELL, 1),
-                    1, 12, 0.05f));
-
+                    new ItemCost(Items.BELL, 1),
+                    new ItemStack(Items.ACTIVATOR_RAIL, 32), 1, 12, 0.05f));
         }
 
 
